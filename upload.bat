62d470ac7493c2de9d5c2f30f235d510a9406067
Chenglin Yin <sdyinruichao@163.com>
2022-06-25 20:32:26 +0800

    脚本更新

--- upload.bat
+++ upload.bat
@@ -10,5 +10,5 @@ scp 1.tar root@149.129.89.106:/root
 del 1.tar
 echo “上传完成,按任意键自动运行服务器目录runweilabweb.sh”
 pause
-ssh root@149.129.89.106:/root "bash runweilabweb.sh"
+ssh root@149.129.89.106 "bash runweilabweb.sh"
 pause
